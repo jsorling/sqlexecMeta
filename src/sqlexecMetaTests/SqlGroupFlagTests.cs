c1c761a152d25bd0e215aa3820b4bb5478b6b21d
Jörgen Sörling <jorgen@sorling.eu>
2023-11-12 22:50:03 +0100

    typeprevnxt

---
+++ src/sqlexecMetaTests/SqlGroupFlagTests.cs
@@ -0,0 +1,13 @@
+﻿using Sorling.SqlExecMeta;
+
+namespace sqlexecMetaTests;
+
+[TestClass]
+public class SqlGroupFlagTests
+{
+   [TestMethod]
+   public void TopLevel() {
+      SqlGroupFlags tl = SqlGroupFlags.TopLevel;
+      Assert.IsTrue(tl.HasFlag(SqlGroupFlags.Tables));
+   }
+}
